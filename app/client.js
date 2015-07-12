c792922ce8440e6a56e378d821262d0411a90302
xpeper <xpepermint@gmail.com>
2015-07-12 16:03:51 +0200

    app client fix (removing test code)

--- app/client.js
+++ app/client.js
@@ -5,9 +5,6 @@ require('./styles/index.styl');
 import alt from './alt';
 alt.bootstrap(__STORES__);
 
-// Starting sockets.
-import sockets from './sockets';
-
 // Rendering react app.
 import React from 'react';
 import Router from 'react-router';
