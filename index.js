79fa998582338b77b23d01bae7f429dd62d69668
xpeper <xpepermint@gmail.com>
2015-07-13 03:11:29 +0200

    replacing ejs with jade

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import assets from 'express-hash-webpack';
+import swig from 'swig';
 import server from './app/server';
 
 let app = express();
 
-app.use(express.static(`${__dirname}/public`));
-
 app.set('views', `${__dirname}/app`);
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use(assets({
   hashPath: './public/assets',
