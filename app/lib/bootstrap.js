939a4e4c02c1bac45af4e20d31658d7f01e4e0b3
xpeper <xpepermint@gmail.com>
2015-07-30 19:06:06 +0200

    minor updates

--- app/lib/bootstrap.js
+++ app/lib/bootstrap.js
@@ -2,15 +2,11 @@ import axios from 'axios';
 import config from '../../config';
 import Locations from '../components/Locations';
 
-export default function(state, options) {
-  let accessToken = options.accessToken;
-
+export default function(state, req) {
   let requests = [];
   if (state.components.includes(Locations)) requests.push({store: 'LocationsStore', url: `${config.apiBaseUrl}/locations`});
 
-  return axios.all(
-    requests.map(r => {return axios.get(r.url, {headers: {'Authorization': `Bearer ${accessToken}`}})})
-  ).then(data => {
+  return axios.all(requests.map(r => {return axios.get(r.url)})).then(data => {
     let stores = {}
     data.forEach((d, i) => {stores[requests[i].store] = d})
     return JSON.stringify(stores);
--- app/server.js
+++ app/server.js
@@ -9,15 +9,14 @@ import Alt from './lib/Alt';
 import bootstrap from './lib/bootstrap';
 
 export default (req, res, next) => {
-  Cookie.setRawCookie(req.headers.cookie||'');
+  Cookie.setRawCookie(req.headers.cookie || '');
 
   let location = new Location(req.path, req.query);
   Router.run(routes, location, (error, state, transition) => {
     if (error) return next(error);
     if (transition.isCancelled) return res.redirect(transition.redirectInfo.pathname);
 
-    let accessToken = req.cookies.accessToken;
-    bootstrap(state, {accessToken}).then(snapshot => {
+    bootstrap(state, req).then(snapshot => {
       Alt.bootstrap(snapshot);
 
       let markup = ReactDOMServer.renderToString(<Router {...state}/>);
